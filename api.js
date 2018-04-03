d9357c046c3c79a4070d0396c081b088fa454039
Arm <upala.piyawat@hotmail.com>
2018-04-03 23:37:24 +0700

    first

--- api.js
+++ api.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const request = require('request');
+const request = require('request-promise');
 const sql = require('mssql');
+const moment = require('moment');
 const _ = require('lodash');
+const async = require('async');
+const googleTrends = require('google-trends-api');
+// const { ExploreTrendRequest, SearchProviders } = require('g-trends')
+// const explorer = new ExploreTrendRequest()
 const config = {
     user: 'WebCSA',
     password: '1SA@webCrawler',
@@ -13,37 +18,167 @@ const config = {
         encrypt: true // Use this if you're on Windows Azure
     }
 }
-
+let fQuery = [];
+require('events').EventEmitter.prototype._maxListeners = 10000;
 module.exports = {
 
-    getAllData: function (res) {
-        return getAll(res);
+    getAllData: function (res, catid) {
+        return getAllData(res, catid);
     }
-    ,getSumData: function(res){
-        return getSumCount(res);
+    , getSumData: function (res) {
+        return getSumData(res);
+    },
+    getCategory: function (res) {
+        return getCategory(res);
+    },
+    addCategory: function (res, category) {
+        return addCategory(res, category);
+    },
+    addKeyword: function (res, keyword, categoryId) {
+        return addKeyword(res, keyword, categoryId);
+    },
+    addBase: function (res) {
+        return addBase(res);
     }
 
 }//end export
 
-let getAll = (res) => {
-    var sqlSelectAll = "SELECT * FROM searchData";
+let getAllData = (res, id) => {
+    if (id > 0 && id < 5) {
+        var sqlSelectAll = "SELECT searchTime,searchCount,locationName FROM searchData WHERE keyword_Id = " + id + " ORDER BY datekey ASC";
+    } else {
+        var sqlSelectAll = "SELECT searchTime,searchCount,locationName FROM searchData WHERE keyword_Id = 1 ORDER BY datekey ASC";
+    }
     sql.close();
     sql.connect(config, err => {
         new sql.Request().query(sqlSelectAll, (sqlSelectAll, result) => {
-            var data = result.recordset;
-            var grouped = _.mapValues(_.groupBy(data, 'locationName'),clist => clist.map(data => _.omit(data, 'locationName')));
-            res.send(grouped);
+            let resultList = [];
+            let countString = '';
+            let timeString = '';
+
+            let data = result.recordset;
+            let dataLength = (result.recordset).length
+            let grouped = _.mapValues(_.groupBy(data, 'locationName'), clist => clist.map(data => _.omit(data, 'locationName')));
+
+            for (let location in grouped) {
+                let countNum = [];
+                let time = [];
+                _.each(grouped[location], item => {
+                    countString += countNum.push(item['searchCount'])
+                    let datetime = moment(item['searchTime']).format('DD-MM-YYYY');
+                    timeString += time.push(datetime)
+
+                })
+                resultList.push({
+                    location: location,
+                    countNum: countNum,
+                    time: time
+                })
+            }
+
+            let dataSend = { resultList }
+            res.send(dataSend);
         })
     });
 }
 
-let getSumCount = (res) => {
-    var sqlSumSelect = "SELECT locationName ,SUM(searchCount) as seachCount FROM searchData GROUP BY locationName";
+let getSumData = (res) => {
+    var sqlSumSelect = "SELECT locationName ,SUM(searchCount) as searchCount FROM searchData GROUP BY locationName";
     sql.close();
     sql.connect(config, err => {
-        new sql.Request().query(sqlSumSelect, (sqlSelectAll, result) => {
-            var data = result.recordset;
-            res.send(data);
+        new sql.Request().query(sqlSumSelect, (sqlSumSelect, result) => {
+            var data = [];
+            data.push({
+                resultList: result.recordset
+            })
+            res.send(data['0']);
         })
     });
 }
+
+let getCategory = (res) => {
+    var sqlSumSelect = "SELECT * FROM Category";
+    sql.close();
+    sql.connect(config, err => {
+        new sql.Request().query(sqlSumSelect, (sqlSumSelect, result) => {
+            var data = [];
+            data.push({
+                resultList: result.recordset
+            })
+            res.send(data['0']);
+        })
+    });
+}
+
+let addCategory = (res, category) => {
+    var sqlInsert = "INSERT INTO Category (category_name) values ('" + category + "')";
+    sql.close();
+    sql.connect(config, err => {
+        new sql.Request().query(sqlInsert, (result) => {
+        })
+    });
+}
+
+let addKeyword = (res, keyword, categoryId) => {
+    var sqlInsert = "INSERT INTO Keyword (keyword_name,category_Id) values ('" + keyword + "'," + categoryId + ")";
+    sql.close();
+    sql.connect(config, err => {
+        new sql.Request().query(sqlInsert, (result) => {
+        })
+    });
+}
+
+let addBase = (res) => {
+    let sqlSelect = "SELECT * FROM Keyword";
+    sql.close();
+    sql.connect(config, err => {
+        new sql.Request().query(sqlSelect, (sqlSelect, result) => {
+            let data = result.recordset;
+            let dataLength = Object.keys(data).length
+            delay_loop(data, dataLength);
+            function delay_loop(items, num) {
+                let data = items.shift(),
+                    // delay between each loop
+                    delay = 2500;
+                // start a delay
+                setTimeout(function () {
+                    // console.log(data.keyword_name)
+                    let s1 = data.keyword_name;
+                    // let s2 = data["0"]["insertQuery"];
+                    // new sql.Request().query(s1, (err, result) => {
+                    //     let count = result.rowsAffected["0"];
+                    //     if (count >= 1) {
+                    //         console.log('Already Data')
+                    //     } else {
+                    //         console.log('Insert Data Success')
+                    //         new sql.Request().query(s2, (err, result) => {
+                    //         })
+                    //     }
+                    // });
+                    // recursive call 
+                    googleTrends.interestOverTime({ keyword: s1 })
+                        .then(function (results) {
+                            console.log(results.default)
+                        })
+                        .catch(function (err) {
+                        });
+                    num--;
+                    if (items.length) {
+                        delay_loop(items, num)
+                    } else {
+                        console.log('Task End')
+                    }
+                }, delay);
+            }
+            // googleTrends.interestOverTime({keyword: 'Women\'s march'})
+            // .then(function(results){
+            //   console.log('These results are awesome', results);
+            // })
+            // .catch(function(err){
+            //   console.error('Oh no there was an error', err);
+            // });
+        })
+    });
+
+
+};
--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const router = express.Router();
 const request = require('request');
 const sql = require('mssql');
+const bodyParser = require('body-parser');
 const schedule = require('node-schedule');
 const { ExploreTrendRequest, SearchProviders } = require('g-trends')
-require('events').EventEmitter.prototype._maxListeners = 10000;
 const explorer = new ExploreTrendRequest()
-const port = process.env.PORT || 8181;
+require('events').EventEmitter.prototype._maxListeners = 10000;
+
+const port = process.env.PORT || 8086;
 const getAPI = require('./api');
 const config = {
     user: 'WebCSA',
@@ -18,80 +20,121 @@ const config = {
         encrypt: true // Use this if you're on Windows Azure
     }
 }
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'appid, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 let location = [];
 let fQuery = [];
+// getAPI.addBase();
 
-let jobSchedule = schedule.scheduleJob('5 0 * * *', function () {
-    sql.close();
-    sql.connect(config, err => {
-        explorer.past30Days()
-            .compare('Wat Phra That Doi Suthep')
-            .compare('Wat Chedi Luang')
-            .compare('Wat Phra Singh')
-            .compare('Wat Umong')
-            .compare('Wat Chiang Man')
-            .download().then(csv => {
-                let dataKeyword = csv["0"].length
-                for (let i = 1; i < dataKeyword; i++) {
-                    let dataLocation = csv["0"][i];
-                    let finalDataLocation = dataLocation.split(':')["0"];
+//  let jobSchedule = schedule.scheduleJob('5 0 * * *', function () {
+//    sql.close();
+//    sql.connect(config, err => {
+//        explorer.past30Days()
+//            .compare('Ginger & The House Shop')
+//            .compare('Sankampaeng Craft Street')
+//            .compare('Central Plaza Chiangmai Airport')
+//            .compare('Warorot Market')
+//            .compare('Central Festival Chiangmai')
+//            .download().then(csv => {
+//                let dataKeyword = csv["0"].length
+//                for (let i = 1; i < dataKeyword; i++) {
+//                    let dataLocation = csv["0"][i];
+//                    let finalDataLocation = dataLocation.split(':')["0"];//
 
-                    for (let x = 1; x < csv.length; x++) {
-                        let locationName = finalDataLocation
-                        let searchTime = csv[x]["0"];
-                        let searchCount = csv[x][i];
-                        let dateKey = ((csv[x]["0"]).split("-")["0"]) + '' + ((csv[x]["0"]).split("-")["1"]) + '' + ((csv[x]["0"]).split("-")["2"]);
-                        let insertQuery = "INSERT INTO searchData (locationName, searchTime, searchCount, datekey) VALUES ('" + locationName + "','" + searchTime + "'," + searchCount + "," + dateKey + ")";
-                        let countQuery = "SELECT * FROM searchData WHERE  locationName = '" + locationName + "' AND searchTime = '" + searchTime + "' ";
-                        fQuery.push([
-                            { insertQuery, countQuery }
-                        ]);
-                    }
-                }
-                let num = fQuery.length - 1;
-                delay_loop(fQuery, num);
-                function delay_loop(items, num) {
-                    let data = items.shift(),
-                        // delay between each loop
-                        delay = 500;
-                    // start a delay
-                    setTimeout(function () {
-                        let s1 = data["0"]["countQuery"];
-                        let s2 = data["0"]["insertQuery"];
-                        new sql.Request().query(s1, (err, result) => {
-                            let count = result.rowsAffected["0"];
-                            if (count >= 1) {
-                                console.log('Already Data')
-                            } else {
-                                console.log('Insert Data Success')
-                                new sql.Request().query(s2, (err, result) => {
-                                })
-                            }
-                        });
-                        // recursive call 
-                        num--;
-                        if (items.length) {
-                            delay_loop(items, num)
-                        } else {
-                            console.log('Task End')
-                        }
-                    }, delay);
-                }
-            });
-    });
+//                    for (let x = 1; x < csv.length; x++) {
+//                        let locationName = finalDataLocation
+//                        let searchTime = csv[x]["0"];
+//                        let searchCount = csv[x][i];
+//                        let dateKey = ((csv[x]["0"]).split("-")["0"]) + '' + ((csv[x]["0"]).split("-")["1"]) + '' + ((csv[x]["0"]).split("-")["2"]);
+//                        let insertQuery = "INSERT INTO searchData (locationName, searchTime, searchCount, datekey,keyword_id) VALUES ('" + locationName + "','" + searchTime + "'," + searchCount + "," + dateKey + ",(SELECT category_Id FROM [Keyword] WHERE keyword_name = '"+locationName+"'))";
+//                        let countQuery = "SELECT * FROM searchData WHERE  locationName = '" + locationName + "' AND searchTime = '" + searchTime + "' ";
+//                        fQuery.push([
+//                            { insertQuery, countQuery }
+//                        ]);
+//                    }
+//                }
+//                let num = fQuery.length - 1;
+//                delay_loop(fQuery, num);
+//                function delay_loop(items, num) {
+//                    let data = items.shift(),
+//                        // delay between each loop
+//                        delay = 500;
+//                    // start a delay
+//                    setTimeout(function () {
+//                        let s1 = data["0"]["countQuery"];
+//                        let s2 = data["0"]["insertQuery"];
+//                        new sql.Request().query(s1, (err, result) => {
+//                            let count = result.rowsAffected["0"];
+//                            if (count >= 1) {
+//                                console.log('Already Data')
+//                            } else {
+//                                console.log('Insert Data Success')
+//                                new sql.Request().query(s2, (err, result) => {
+//                                })
+//                            }
+//                        });
+//                        // recursive call 
+//                        num--;
+//                        if (items.length) {
+//                            delay_loop(items, num)
+//                        } else {
+//                            console.log('Task End')
+//                        }
+//                    }, delay);
+//                }
+//            });
+//    });
+//  });
+
+router.use(function (req, res, next) {
+    next();
 });
 
-app.get('/api/allData', function (req, res) {
-	getAPI.getAllData(res);
-})
+router.route('/data/:id')
+    .get(function (req, res) {
+        let catid = req.params.id
+        console.log(catid)
+        getAPI.getAllData(res, catid);
+    })
 
-app.get('/api/sumData', function (req, res) {
-	getAPI.getSumData(res);
-})
+router.route('/sumdata')
+    .get(function (req, res) {
+        getAPI.getSumData(res);
+    })
 
+router.route('/keyword')
+    .post(function (req, res) {
+        getAPI.addKeyword(res, keyword);
+        res.end('save success');
+    })
 
+router.route('/category')
+    .post(function (req, res) {
+        let category = req.body.category;
+        getAPI.addCategory(res, category);
+        res.end('save success');
+    })
+
+    .get(function (req, res) {
+        getAPI.getCategory(res);
+    })
+router.route('/testing')
+
+    .get(function (req, res) {
+        getAPI.addBase(res);
+    })
+
+app.use('/api', router);
 
 app.listen(port, function () {
-    console.log("App start on http://localhost:8181");
+    console.log("App start on http://localhost:8086");
 });
